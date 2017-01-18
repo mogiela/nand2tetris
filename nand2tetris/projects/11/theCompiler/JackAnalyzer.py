e2d04457504924ad5798cc3b6ac4c849440e305f
almog <mogiela@gmail.com>
2017-01-18 21:17:53 +0000

    JackAnalyzer.py edited online with Bitbucket

--- nand2tetris/projects/11/theCompiler/JackAnalyzer.py
+++ nand2tetris/projects/11/theCompiler/JackAnalyzer.py
@@ -6,28 +6,27 @@ from Tokenizer import Tokenizer
 
 def Manage(filesList):
     for path in filesList:
-        head, tail = os.path.split(path)
-        outputPath = '%s/%s.xml' % (head, tail[:tail.rfind('.')])
-        print(outputPath)
-        tokenizedFile = Tokenizer(path)
-        compileEngine = CompilationEngine(tokenizedFile, outputPath)
+        tokonizedFile = Tokenizer(path)
+        outputPath = path[:-4] + "vm"
+        compileEngine = CompilationEngine(tokonizedFile, outputPath)
         compileEngine.compileClass()
-        tokenizedFile.closeFile()
+        tokonizedFile.closeFile()
         compileEngine.closeFile()
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 2:
-        raise ValueError("Wrong number of arguments")
-    path = os.path.realpath(sys.argv[1])
-    files = []
-    outputPath = ""
-    if os.path.isdir(path):
-        # get all files from dir and add them to the files list
-        for filename in os.listdir(path):
-            if filename.endswith(".jack"):
-                files.append(path + "/" + filename)
-    else:
-        files.append(path)
+if len(sys.argv) > 2:
+    raise ValueError("Wrong number of arguments")
+path = sys.argv[1]
+files = []
+outputPath = ""
+if os.path.isdir(path):
+    # get all files from dir and add them to the files list
+    for file in os.listdir(path):
+        if file[-4:] == "jack":
+            files.append((path + "/" + file))
 
-    Manage(files)
+
+else:
+    files.append(path)
+
+Manage(files)
